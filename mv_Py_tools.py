c5d298dbc6de90f879a5c593abb5d998c30fb9e5
Fang Jianqi <fllypanda@outlook.com>
2018-07-05 08:31:40 +0800

    fix some bugs:1.change the monitor logic 2.change to py tools from forground to background

--- mv_Py_tools.py
+++ mv_Py_tools.py
@@ -1,7 +1,7 @@
 import os,sys
 if sys.argv[1]=="start":
-    os.system("python mvknow_app.py")
-    os.system("python monitor_mission.py")
+    os.system("nohup python mvknow_app.py &")
+    os.system("nohup python monitor_mission.py &")
     print("启动成功")
 elif sys.argv[1]=="stop":
     os.system("pkill python ")
