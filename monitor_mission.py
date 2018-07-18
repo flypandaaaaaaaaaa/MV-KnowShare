3e47393c4bf3b712ddfb529cad2135c24f4a076c
Fang Jianqi <fllypanda@outlook.com>
2018-07-18 13:28:16 +0800

    fix bug

--- monitor_mission.py
+++ monitor_mission.py
@@ -2,7 +2,7 @@ from MySQL_Modules import article,del_article
 from mvknow_init import db
 import time
 from mvknow_func import GetNowTime
-
+from move_article import mv_article
 while True:
     time.sleep(10)
     print('开始自动下线有阅读次数限制的文章')
@@ -10,13 +10,8 @@ while True:
     for i in Article_list:
         if i.read_limit is None:
             print("无阅读次数限制",GetNowTime(),i.title)
-        elif i.read_num >= i.read_limit:
-            del_article_row = del_article(id=i.id, title=i.title, body=i.body,
-                                          created_date=i.created_date, author=i.author,
-                                          read_num=i.read_num, abstract=i.abstract,read_limit=i.read_limit,read_public=i.read_public)
-            db.session.add(del_article_row)
-            db.session.delete(i)
-            db.session.commit()
+        elif int(i.read_num) >= int(i.read_limit):
+            mv_article(i.id).offline_article()
             print('文章到达阅读次数限制，下线成功',GetNowTime(),i.title)
         else:
             print('文章还未到达阅读次数限制',GetNowTime(),i.title)
